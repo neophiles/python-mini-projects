7d8e2aa50061b27dd69a2f5be179aca9fa1a7d64
neophiles <mn.tagle.cs@gmail.com>
2025-06-14 09:47:28 +0800

    refactor: rearrange layout and resize window

--- unit-converter/main.py
+++ unit-converter/main.py
@@ -11,12 +11,12 @@ class App(tk.Tk):
     def __init__(self):
         super().__init__() # inherits from tk.Tk
         self.title('Unit Converter') # defines window title
-        self.geometry('500x200+0+0') # defines window dimensions
+        self.geometry('300x300+0+0') # defines window dimensions
         self.resizable(0, 0) # disables window resizing
 
         # define text fonts
-        self.normat_text = font.Font(family="Helvetica", size=10)
-        self.bold_text = font.Font(family="Helvetica", size=10, weight='bold')
+        self.normat_text = font.Font(family="Helvetica", size=12)
+        self.bold_text = font.Font(family="Helvetica", size=12, weight='bold')
 
         # instantiate UnitConverter
         self.unit_converter = UnitConverter()
@@ -27,6 +27,7 @@ class App(tk.Tk):
         self.create_header_frame_widgets()
         self.create_input_frame_widgets()
         self.create_output_frame_widgets()
+        self.create_footer_frame_widgets()
         self.reset_fields()
 
     def create_containers(self):
@@ -34,23 +35,28 @@ class App(tk.Tk):
         # define window grid layout
         self.grid_rowconfigure(0, weight=1)
         self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure(2, weight=1)
         self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=1)
+        #self.grid_columnconfigure(1, weight=1)
 
         # create window frames
-        self.header_frame = tk.Frame(self, width=500, height=50)
-        self.input_frame = tk.Frame(self, width=250, height=150)
-        self.output_frame = tk.Frame(self, width=250, height=150)
+        # highlightbackground='black', highlightthickness=1
+        self.header_frame = tk.Frame(self, width=300, height=50)
+        self.input_frame = tk.Frame(self, width=300, height=100)
+        self.output_frame = tk.Frame(self, width=300, height=100)
+        self.footer_frame = tk.Frame(self, width=300, height=50)
 
         # place frames on window grid
-        self.header_frame.grid(row=0, column=0, columnspan=2, padx=5, pady=5, sticky='nsew')
+        self.header_frame.grid(row=0, column=0, padx=5, pady=5, sticky='nsew')
         self.input_frame.grid(row=1, column=0, padx=5, pady=5, sticky='nsew')
-        self.output_frame.grid(row=1, column=1, padx=5, pady=5, sticky='nsew')
+        self.output_frame.grid(row=2, column=0, padx=5, pady=5, sticky='nsew')
+        self.footer_frame.grid(row=3, column=0, padx=5, pady=5, sticky='nsew')
         
         # keep frame sizes
         self.header_frame.grid_propagate(False)
         self.input_frame.grid_propagate(False)
         self.output_frame.grid_propagate(False)
+        self.footer_frame.grid_propagate(False)
 
     def create_header_frame_widgets(self):
         # create and place title label
@@ -62,20 +68,16 @@ class App(tk.Tk):
         # create input label and field
         self.input_value=tk.StringVar()
         self.input_label = tk.Label(self.input_frame, text='From:', font=self.bold_text)
-        self.input_entry = tk.Entry(self.input_frame, textvariable=self.input_value, font=self.normat_text)
+        self.input_entry = tk.Entry(self.input_frame, textvariable=self.input_value, font=self.normat_text, width=20)
         
         # create input unit dropdown
         self.input_unit=tk.StringVar()
-        self.input_unit_cbbx = ttk.Combobox(self.input_frame, values=self.units, textvariable=self.input_unit, width=10, state='readonly')
-
-        # create reset fields button
-        self.reset_btn = tk.Button(self.input_frame, text='Clear', command=self.reset_fields)
+        self.input_unit_cbbx = ttk.Combobox(self.input_frame, values=self.units, textvariable=self.input_unit, width=12, state='readonly')
 
         # place widgets on frame grid
         self.input_label.grid(row=1, column=0, padx=5, pady=5, sticky='w')
         self.input_entry.grid(row=2, column=0, padx=(5, 0), pady=5, sticky='nsew')
         self.input_unit_cbbx.grid(row=2, column=1, padx=(0, 5), pady=5, sticky='nsew')
-        self.reset_btn.grid(row=3, column=0, padx=5, pady=5, sticky='w')
 
         # bind combobox event
         self.input_unit_cbbx.bind('<<ComboboxSelected>>', self.input_unit_selected)
@@ -85,23 +87,29 @@ class App(tk.Tk):
         # create output label and field
         self.output_value=tk.StringVar()
         self.output_label = tk.Label(self.output_frame, text='To:', font=self.bold_text)
-        self.output_entry = tk.Entry(self.output_frame, textvariable=self.output_value, font=self.normat_text, state='readonly')
+        self.output_entry = tk.Entry(self.output_frame, textvariable=self.output_value, font=self.normat_text, width=20, state='readonly')
 
         # create output unit dropdown
         self.output_unit=tk.StringVar()
-        self.output_unit_cbbx = ttk.Combobox(self.output_frame, values=self.units, textvariable=self.output_unit, width=10, state='readonly')
-
-        # create convert button
-        self.convert_btn = tk.Button(self.output_frame, text='Convert', command=self.convert)
+        self.output_unit_cbbx = ttk.Combobox(self.output_frame, values=self.units, textvariable=self.output_unit, width=12, state='readonly')
 
         # place widgets on frame grid
         self.output_label.grid(row=1, column=0, padx=5, pady=5, sticky='w')
         self.output_entry.grid(row=2, column=0, padx=(5, 0), pady=5, sticky='nsew')
         self.output_unit_cbbx.grid(row=2, column=1, padx=(0, 5), pady=5, sticky='nsew')
-        self.convert_btn.grid(row=3, column=0, padx=5, pady=5, sticky='w')
 
         # bind combobox event
         self.output_unit_cbbx.bind('<<ComboboxSelected>>', self.output_unit_selected)
+    
+    def create_footer_frame_widgets(self):
+
+        # create reset fields and convert button
+        self.reset_btn = tk.Button(self.footer_frame, text='Clear', command=self.reset_fields)
+        self.convert_btn = tk.Button(self.footer_frame, text='Convert', command=self.convert)
+
+        # place buttons on the frame grid
+        self.reset_btn.grid(row=0, column=0, padx=5, pady=5, sticky='w')
+        self.convert_btn.grid(row=0, column=1, padx=5, pady=5, sticky='w')        
 
     def input_unit_selected(self, event):
         selected = self.input_unit_cbbx.get()
@@ -113,17 +121,23 @@ class App(tk.Tk):
 
     def convert(self):
         try:
-            
+
             # retrieves user input in fields
-            input_value = int(self.input_value.get())
+            input_value = self.input_value.get()
             input_unit = self.input_unit.get()
             output_unit = self.output_unit.get()
 
-            # converts units
-            result = self.unit_converter.convert(input_value, input_unit, output_unit)
+            # checks if all fields are filled
+            if input_value != '' and all(unit in self.units for unit in [input_unit, output_unit]):
 
-            # displays conversion in the output value field
-            output_value = self.output_value.set(result)
+                # converts units
+                result = self.unit_converter.convert(int(input_value), input_unit, output_unit)
+
+                # displays conversion in the output value field
+                output_value = self.output_value.set(result)
+
+            else:
+                print("All fields not filled.")
 
         except Exception as e:
             print(f'Unexpected error: {e}')
