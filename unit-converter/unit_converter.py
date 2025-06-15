1ce9ec5289679f185e69dd53a5b40598194628e4
neophiles <mn.tagle.cs@gmail.com>
2025-06-15 17:59:02 +0800

    feat: support mass and volume unit conversion
    - Refactored UnitConverter to support three measurement types: length, mass, and volume.
    - Replaced hardcoded conversion dictionaries with a generalized measurement_types list.
    - Added set_measurement_type() to configure the converter based on selected type.
    - Used math.prod() to simplify logic in cross-system conversions.
    - Improved modularity and scalability for future unit additions.

--- unit-converter/unit_converter.py
+++ unit-converter/unit_converter.py
@@ -2,54 +2,115 @@
 # Created by Marc Neil Tagle, June 2025
 # For educational purposes
 
+import math
+
 class UnitConverter:
     
     def __init__(self):
         
-        # metric conversion factors with meters as the base unit
-        self.metric_conversion_factors = {
-            'mm': 1000,
-            'cm': 100,
-            'm': 1,
-            'km': 0.001
-        }
+        # list of dictionaries containg basic units of measurements
+        self.measurement_types = [
 
-        # imperial conversion factors with feet as the base unit
-        self.imperial_conversion_factors = {
-            'in': 12,
-            'ft': 1,
-            'yd': 1/3,
-            'mi': 1/5280
-        }
+            # length metric units
+            {
+                'mm': 1000,
+                'cm': 100,
+                'm': 1,
+                'km': 0.001
+            },
+
+            # length imperial units
+            {
+                'in': 12,
+                'ft': 1,
+                'yd': 1/3,
+                'mi': 1/5280
+            },
+
+            # weight metric units
+            {
+                'mg': 1000,
+                'g': 1,
+                'kg': 1/1000,
+                't': 1/1000000
+            },
+
+            # weight imperial units
+            {
+                'oz': 16,
+                'lb': 1,
+                'st': 1/14
+            },
+
+            # volume metric units
+            {
+                'mL': 1000,
+                'L': 1
+            },
+
+            # volume imperial units
+            {
+                'fl oz': 128,
+                'pt': 8,
+                'qt': 4,
+                'gal': 1
+            }
+
+        ]
+
+        # initialize other class attributes
+        self.type = ''
+        self.type_systems = []
+        self.CONVERSION_CONSTANT = 0
+    
+    def set_measurement_type(self, type):
+        self.type = type # sets measurement type
+
+        # determines corresponding index in self.measurement_types and cross-system conversion constant
+        if self.type == 'length':
+            self.type_systems = self.measurement_types[0:2]
+            self.CONVERSION_CONSTANT = 3.280839895013123 
+        elif self.type == 'mass':
+            self.type_systems = self.measurement_types[2:4]
+            self.CONVERSION_CONSTANT = 453.59237
+        elif self.type == 'volume':
+            self.type_systems = self.measurement_types[4:6]
+            self.CONVERSION_CONSTANT = 3.785411784
 
     def get_units(self):
         # returns a list of all units in all system dictionaries
-        return list(self.metric_conversion_factors) + (list(self.imperial_conversion_factors))
+        return list(unit for system in self.type_systems for unit in system.keys())        
+    
+    def identify_measurement_system(self, unit):
+        # determines the system of selected unit based on the measurement type
+        for system in self.type_systems:
+            if unit in system.keys():
+                return system
 
-    def convert(self, length, unit_from, unit_to):
+    def convert(self, value, unit_from, unit_to):
 
-        # metric-imperial conversion constants
-        METER_TO_FOOT = 3.28084
-        FOOT_TO_METER = 0.3048
+        # gets measurement system of starting unit
+        system = self.identify_measurement_system(unit_from)
 
-        # checks the conversion system the starting unit is in
-        system = self.metric_conversion_factors
-        other = self.imperial_conversion_factors
-        if unit_from in self.imperial_conversion_factors.keys():
-            system = self.imperial_conversion_factors
-            other = self.metric_conversion_factors
-
-        basic_length = length / system[unit_from] # converts raw length to its basic unit
+        # converts raw length to its basic system unit value
+        basic_value = value / system[unit_from] 
         
         # checks if starting and desired units are from the same or different conversion systems
-        if unit_to not in system: 
-            if unit_to in self.metric_conversion_factors.keys():
-                converted = basic_length * FOOT_TO_METER
+        if unit_to not in system:
+            other = self.identify_measurement_system(unit_to)
+
+            # i don't know if this is big brain move (i just found a way)
+            ''' if product of system's conversion values > that of counterpart system, basic unit value divided by constant;
+                otherwise, it is multiplied by the constant '''
+            if math.prod(other.values()) > math.prod(system.values()):
+                converted = basic_value / self.CONVERSION_CONSTANT
             else:
-                converted = basic_length * METER_TO_FOOT
-            converted *= other[unit_to]
+                converted = basic_value * self.CONVERSION_CONSTANT
+
+            converted *= other[unit_to] # converts cross-system converted value to user's desired unit
+
         else:
-            converted = basic_length * system[unit_to]
+            converted = basic_value * system[unit_to] # directly converts basic unit value to user's desired unit
 
         # removes trailing zero decimal if result is a whole number
         if converted % 1 == 0:
